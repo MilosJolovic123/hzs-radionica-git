a17c4f6086e67c57cff81d7e662db98a427ee3fd
milos-jolovic <mj20200179@student.fon.bg.ac.rs>
2024-12-01 12:54:59 +0100

    Povukao sam promene iz stastha i omogucio da se one commituju

--- reactapp/src/Feature.jsx
+++ reactapp/src/Feature.jsx
@@ -4,6 +4,9 @@ const Feature = () => {
   return (
     <div>
         <p>ovde cu da radim na nekom mom novom featuru</p>
+        <p>ovde i dalje radim na featuru</p>
+        <p>nastavlja da radim na featuru</p>
+        <p>zavrsim rad na tom nekom featuru</p>
     </div>
   )
 }
