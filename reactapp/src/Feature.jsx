4661e345d37291020c38e4034dc5e48dd0c9d801
milos-jolovic <mj20200179@student.fon.bg.ac.rs>
2024-12-01 12:47:57 +0100

    ja radim na featuru

---
+++ reactapp/src/Feature.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <div>
+        <p>ovde cu da radim na nekom mom novom featuru</p>
+    </div>
+  )
+}
+
+export default Feature
