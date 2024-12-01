7d557ec667d56afd251139bf758f118eb886e57b
milos-jolovic <mj20200179@student.fon.bg.ac.rs>
2024-12-01 12:30:16 +0100

    Odradili smo merge grane login!

---
+++ reactapp/src/LoginComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginComponent = () => {
+  return (
+    <div>
+        <p>Ovo ce biti nasa komponenta za login stranu!</p>
+    </div>
+  )
+}
+
+export default LoginComponent
