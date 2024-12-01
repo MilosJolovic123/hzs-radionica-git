b514f512393f6763e0be7cf85f3ae0586366ae50
milos-jolovic <mj20200179@student.fon.bg.ac.rs>
2024-12-01 12:20:04 +0100

    Sad smo pokazali kako rade i git diff i git diff --cached

--- reactapp/src/ThirdComponent.jsx
+++ reactapp/src/ThirdComponent.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ThirdComponent = () => {
   return (
-    <div>ThirdComponent</div>
+    <div>
+        <p>git je super alat i sad cemo da pokazemo sta radi git diff bez opcije --cached!</p>
+    </div>
   )
 }
 
